6b1cb688250eac953fc3e626a39c5c7afb5966b4
왕환민 <mini_wang@naver.com>
2024-12-30 19:06:30 +0900

    feat:[왕환민]로그인 배너 구현[RBF-1]

---
+++ ui/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-app>
+      <NavigationBar />
+      <v-main>
+        <NuxtPage />
+      </v-main>
+    </v-app>
+  </v-container>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
